29f5e23aa356979a4ba8832d6cebd89bb41224a1
kurniawantaari <lestari.ekowahyu@gmail.com>
2016-05-26 12:07:36 +0700

    add note and try dialog gui in tcl tk

--- GUI.R
+++ GUI.R
@@ -1,3 +1,14 @@
 ##
 # Tugas APG - TclTk - Visualisasi
 # Komputasi Statistik
+require (tcltk)
+welcomeDialogue <- function(){
+  
+  tt <- tktoplevel()
+  tkwm.title(tt,"Visualisasi")
+  tes<-"This is tes page gui for visualisasi."
+  tkgrid(tklabel(tt,text=tes), pady = 10)
+  
+}
+
+welcomeDialogue()
